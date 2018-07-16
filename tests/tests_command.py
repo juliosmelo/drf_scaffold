2456c0f4c0d0ab71f576c6a5e904166a4ebeec53
Julio Melo <juliocsmelo@gmail.com>
2018-07-16 20:30:00 -0300

    test: test improvements

--- tests/tests_command.py
+++ tests/tests_command.py
@@ -21,3 +21,12 @@ class CommandTestCase(SimpleTestCase):
     def test_command(self):
         call_command('startdrfapp', 'test_drfscafolld', '-m', 'my_model', '--fields', 'name:string, username:string, age:integer, id:uuid')
         self.assertTrue(os.path.exists('./test_drfscafolld'))
+        self.assertTrue(os.path.exists('./test_drfscafolld/models.py'))
+        self.assertTrue(os.path.exists('./test_drfscafolld/views.py'))
+        self.assertTrue(os.path.exists('./test_drfscafolld/urls.py'))
+        self.assertTrue(os.path.exists('./test_drfscafolld/apps.py'))
+        self.assertTrue(os.path.exists('./test_drfscafolld/admin.py'))
+        self.assertTrue(os.path.exists('./test_drfscafolld/serializers.py'))
+        self.assertTrue(os.path.exists('./test_drfscafolld/__init__.py'))
+        self.assertTrue(os.path.exists('./test_drfscafolld/migrations'))
+        self.assertTrue(os.path.exists('./test_drfscafolld/migrations/__init__.py'))
