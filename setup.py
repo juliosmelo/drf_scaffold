aae1595cade661427a525d5a51d5508f90d72526
Julio Melo <juliocsmelo@gmail.com>
2018-07-16 19:44:01 -0300

    misc: adjust description

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ def get_version(filename):
 
 NAME = 'drf_scafolld'
 VERSION = get_version(os.path.join('drf_scafolld', '__init__.py'))
-DESCRIPTION = 'Dead simple custom command scafolld to create a django app with djando rest framework CRUD RESTFull API'
+DESCRIPTION = 'Dead simple custom command to create a django app with djando rest framework CRUD RESTFull API'
 URL = 'https://github.com/juliosmelo/drf_scafolld'
 AUTHOR = 'juliocsmelo'
 AUTHOR_EMAIL = 'juliocsmelo@gmail.com'
